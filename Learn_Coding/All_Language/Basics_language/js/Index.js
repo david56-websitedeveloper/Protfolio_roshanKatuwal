1b21f493d2292405748ed401e399ac5e9da1e0fc
Roshan <roshankatuwal906@gmail.com>
2025-03-05 21:47:14 -0500

    cHnages

---
+++ Learn_Coding/All_Language/Basics_language/js/Index.js
@@ -0,0 +1,19 @@
+// My first JavaScript code
+console.log("Hello Basic of JavaScript");
+console.log("I love you Kanxi");
+console.log("I love Hindi movies and everybody");
+
+// Explanation of JavaScript
+console.log("What is JavaScript?\nJavaScript is a programming language used for making web pages interactive. It runs inside web browsers and can control HTML and CSS. JavaScript can change or update the content of an HTML document, change the style of the HTML document, and react to user actions. JavaScript is an essential web technology, along with HTML and CSS, and most websites use it. JavaScript is a high-level programming language, which means it is a language that is closer to human language than machine language. It is a language that is easy to read and write. JavaScript is a versatile language and is used for many different things, such as web development, game development, and mobile app development. JavaScript is a client-side language, which means it runs on the user's computer, not on the server. This makes it fast and responsive, as it does not have to communicate with the server every time it needs to do something. JavaScript is an interpreted language, which means it is executed line by line, as opposed to compiled languages, which are executed all at once.");
+
+
+// Second JavaScript code
+alert("Hello Basic of JavaScript");
+
+
+// Alerts
+alert("I love you Kanxi");
+alert("I love Hindi movies and everybody");
+
+// Third JavaScript code
+console.log("This is the third JavaScript script running!");
